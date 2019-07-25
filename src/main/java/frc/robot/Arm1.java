8bfc45c92bad20bdfd1e89eeaeee93e36f47587a
Sean <seantierney42@gmail.com>
2019-07-25 14:49:43 -0700

    Add limit switch to Arm1

--- src/main/java/frc/robot/Arm1.java
+++ src/main/java/frc/robot/Arm1.java
@@ -2,16 +2,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Arm1 {
     private final VictorSP rotator;
     private final Solenoid extender;
     private boolean extenderState;
 
-    public Arm1(int rotatorPort, int extenderPort) {
+    private DigitalInput limitSwitch;
+
+    public Arm1(int rotatorPort, int extenderPort, int limitSwitchPort) {
         rotator = new VictorSP(rotatorPort);
         extender = new Solenoid(extenderPort);
         extenderState = false;
+
+        limitSwitch = new DigitalInput(limitSwitchPort);
     }
 
     public void setRotator(double power) {
@@ -25,5 +30,9 @@ public class Arm1 {
 
     public void changeExtenderState() {
         setExtender(!extenderState);
+    }
+
+    public boolean getLimitSwitch() {
+        return limitSwitch.get();
     }
 }
