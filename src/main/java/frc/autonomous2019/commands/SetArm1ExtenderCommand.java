8f897a36c9067a074fa5e1c5f2915c992856cbb5
Sean <seantierney42@gmail.com>
2019-07-31 22:10:45 -0700

    Make SetArm1ExtenderCommand.

---
+++ src/main/java/frc/autonomous2019/commands/SetArm1ExtenderCommand.java
@@ -0,0 +1,27 @@
+package frc.autonomous2019.commands;
+
+import frc.autonomous.Command;
+import frc.robot.Arm1;
+
+public class SetArm1ExtenderCommand implements Command {
+
+    boolean out;
+    Arm1 arm1;
+
+    public SetArm1ExtenderCommand(boolean out, Arm1 arm1) {
+        this.out = out;
+        this.arm1 = arm1;
+
+        reset();
+    }
+
+    @Override
+    public boolean run() {
+        arm1.setExtender(out);
+        return true;
+    }
+
+    @Override
+    public void reset() {}
+
+}
