821024d5b14c2a786e08b9f6be4a59848647b634
Sean <seantierney42@gmail.com>
2019-07-31 22:15:24 -0700

    Add arm1 rotate and extend commands to command factory.

--- src/main/java/frc/autonomous2019/commands/CommandFactory2019.java
+++ src/main/java/frc/autonomous2019/commands/CommandFactory2019.java
@@ -2,16 +2,27 @@ package frc.autonomous2019.commands;
 
 import frc.autonomous.Command;
 import frc.autonomous.commands.CommandFactory;
+import frc.robot.Arm1;
 import frc.robot.DriveBase;
 
 public class CommandFactory2019 extends CommandFactory {
 
     DriveBase driveBase;
+    Arm1 arm1;
 
-    public CommandFactory2019 (DriveBase driveBase) {
+    public CommandFactory2019 (DriveBase driveBase, Arm1 arm1) {
         this.driveBase = driveBase;
+        this.arm1 = arm1;
     }
     public Command moveStraight(double time, double power, boolean stop) {
         return new MoveStraightCommand(power, time, driveBase, stop);
+    }
+
+    public Command rotateArm1Command(double time, double power) {
+        return new RotateArm1Command(power, time, arm1);
+    }
+
+    public Command setArm1Extender(boolean out) {
+        return new SetArm1ExtenderCommand(out, arm1);
     }
 }
