dddb9ca486487741f69675c7dee4d6ca2fc91663
Sean <seantierney42@gmail.com>
2019-08-01 18:34:28 -0700

    Add changeExtenderState to Arm2 and Arm3.

--- src/main/java/frc/robot/Arm2.java
+++ src/main/java/frc/robot/Arm2.java
@@ -10,12 +10,15 @@ public class Arm2
     private final Solenoid extender;
     private final DigitalInput limitSwitch;
 
+    private boolean extenderState;
+
     public Arm2(int rotatorPort, int extenderPort, int limitSwitchPort)
     {
         rotator = new VictorSP(rotatorPort);
         extender = new Solenoid(extenderPort);
-
         limitSwitch = new DigitalInput(limitSwitchPort);
+
+        extenderState = false;
     }
 
     public void setRotator(double power)
@@ -27,6 +30,11 @@ public class Arm2
     public void setExtender(boolean out)
     {
         extender.set(out);
+        extenderState = out;
+    }
+
+    public void changeExtenderState() {
+        setExtender(!extenderState);
     }
 
     public boolean getlimitSwitch()
--- src/main/java/frc/robot/Arm3.java
+++ src/main/java/frc/robot/Arm3.java
@@ -9,14 +9,23 @@ public class Arm3 {
     private final Solenoid extender;
     private final DigitalInput limitSwitch;
 
+    private boolean extenderState;
+
     public Arm3(int rotatorChannel, int extenderChannel, int limitSwitchChannel) {
         rotator = new VictorSP(rotatorChannel);
         extender = new Solenoid(extenderChannel);
         limitSwitch = new DigitalInput(limitSwitchChannel);
+
+        extenderState = false;
     }
 
     public void setExtender(boolean state) {
         extender.set(state);
+        extenderState = state;
+    }
+
+    public void changeExtenderState() {
+        setExtender(!extenderState);
     }
 
     public void setRotator(double speed) {
